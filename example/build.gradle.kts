7dd932a89c6dff0df30267fde20d5d7db0c8e814
Bartosz Galek <bartosz@galek.com.pl>
2024-11-18 00:58:11 +0100

    locking scio-core versions, version catalog, and scalacheck handling

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -6,26 +6,9 @@ dependencies {
     implementation(platform(project(":our-platform"))) // bom that is extending original scio-bom
     implementation("com.spotify:scio-cassandra3_2.13") // original dependency, not changed by our constraints (0.14.9)
     implementation("com.spotify:scio-redis_2.13") // overridden version from scio-bom but changed to ours (0.14.9 -> 0.14.7)
-    implementation("args4j:args4j") // overridden version from scio-bom but changed to ours (2.33 -> 2.32)
+    implementation("args4j:args4j") // version from scio-core, cannot be changed
+    implementation("com.fasterxml.jackson.core:jackson-databind") // version from scio-core, cannot be changed, TODO: try to set it to 2.18.1 (latest)
     implementation("com.github.bgalek.security.svg:safe-svg") // custom version from our bom, no need to specify (1.1.4)
     implementation("com.spotify:scio-core_2.13") // version from bom, no need to specify (0.14.9), will be overridden by our constraints
-}
-
-// just for demo purposes
-tasks.register("demo") {
-    doLast {
-        println("args4j: " + getVersion("args4j:args4j"))
-        println("cassandra3: " + getVersion("com.spotify:scio-cassandra3_2.13"))
-        println("scio-redis: " + getVersion("com.spotify:scio-redis_2.13"))
-        println("safe-svg: " + getVersion("com.github.bgalek.security.svg:safe-svg"))
-    }
-}
-
-// just for demo purposes
-fun getVersion(coordinates: String): String {
-    val split = coordinates.split(":", limit = 2)
-    return configurations.runtimeClasspath.get()
-        .resolvedConfiguration
-        .firstLevelModuleDependencies
-        .find { it.moduleGroup == split[0] && it.moduleName == split[1] }!!.moduleVersion
+    testImplementation("org.scalatestplus:scalacheck-1-18_3") // dynamic version based on scala-test dependency
 }
--- our-platform/build.gradle.kts
+++ our-platform/build.gradle.kts
@@ -6,32 +6,69 @@ javaPlatform {
     allowDependencies()
 }
 
+val scalaTestAware = configurations.create("scalaTestAware") {
+    isCanBeResolved = true
+    isCanBeConsumed = false
+}
+
+val versionAware = configurations.create("versionAware") {
+    isCanBeResolved = true
+    isCanBeConsumed = false
+}
+
+fun resolveVersion(group: String, name: String) = scalaTestAware
+    .resolvedConfiguration
+    .resolvedArtifacts
+    .first { it.moduleVersion.id.group == group && it.name == name }
+    .moduleVersion
+    .id
+    .version
+
 dependencies {
-    api(platform("com.spotify:scio-bom:0.14.9"))
-    api("com.spotify:scio-core_2.13:0.14.9")
+    // dependencies that will go to the runtime classpath of users
+    api(platform(libs.scio.bom))
+    api(libs.scio.core)
+
+    // dependencies that are only here to be scanned
+    scalaTestAware(libs.scio.test)
+    versionAware(libs.scio.core)
+
     constraints {
-        api("com.spotify:scio-core_2.13") {
-            version {
-                strictly("0.14.9")
+        // make sure that all dependencies from the scio-core are strictly matching sci-core version
+        versionAware
+            .resolvedConfiguration
+            .resolvedArtifacts
+            .forEach {
+                api("${it.moduleVersion.id.group}:${it.moduleVersion.id.name}") {
+                    version { strictly(it.moduleVersion.id.version) }
+                }
             }
+
+        // make sure that scalacheck from the scio-test is strictly matching sci-test version
+        api("org.scalatestplus:scalacheck-1-18_3") {
+            val scalaTestVersion = resolveVersion("org.scalatest", "scalatest_2.13")
+            version { strictly("$scalaTestVersion.0") }
         }
+
+        // add custom constraint, that is absent in the scio-bom
         api("com.github.bgalek.security.svg:safe-svg") {
             version {
-                // next line sets our own constraint, that is absent in scio-bom
                 strictly("1.1.4")
             }
         }
+
+        // override the version from the scio-bom or scio-core (0.14.9)
         api("com.spotify:scio-redis_2.13") {
             version {
-                // next line it will effectively override the version from the scio BOM (0.14.9)
                 strictly("0.14.7")
             }
         }
-        api("args4j:args4j") {
-            version {
-                // next line it will effectively override the version from the scio BOM (2.33)
-                strictly("2.32")
-            }
-        }
+
+        // overriding a transient library version from the scio-core will fail since it's already locked by scio-core
+//        api("args4j:args4j") {
+//            version {
+//                strictly("2.33")
+//            }
+//        }
     }
 }
